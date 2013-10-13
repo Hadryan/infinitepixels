520f61244225d46aba87406510c2d544c1ce64e7
Joshua Davis <jdavis@colormepixeled.com>
2013-10-12 21:01:31 -0700

    Reorg JS and CSS out of main index

--- js/app.js
+++ js/app.js
@@ -1,37 +1,158 @@
-App = Ember.Application.create();
+var App = {};
 
-App.Router.map(function() {
-  // put your routes here
+var Image = Backbone.Model.extend({
 });
 
-App.Photo = DS.Model.extend({
-	category: DS.attr('number'),
-	image_url: DS.attr('string'),
-	name: DS.attr('string'),
-	nsfw: DS.attr('nsfw'),
-	safeImage: function() {
-		//TODO return work safe versions if an option is set
-		return this.get('image_url');
+var ImageView = Backbone.View.extend({
+	tagName: "li",
+	template: _.template($('#template-image').html()),
+	events: {
+	},
+	initialize: function() {
+		this.listenTo(this.model, 'change', this.render);
+		this.listenTo(this.model, 'remove', this.remove);
+	},
+	render: function() {
+		this.$el.html(this.template(this.model.toJSON()));
+		return this;
+	}
+});
+var LightboxImageView = ImageView.extend({
+	tagName: "div",
+	template: _.template($('#template-lightbox-image').html()),
+	initialize: function(){},
+	events: {
+		"click .image-target": "hideLightbox"
+	},
+	hideLightbox: function() {
+		$('#lightbox').empty();
+		history.back();
 	}
 });
 
-App.store = DS.Store.extend({
-	 findQuery: function(what, params) {
-		var store = this.get('store');
-		var pluralized = what + 's';
-
-		_500px.api('/' + pluralized, params, function (response) {
-			store.pushMany(response[pluralized]);
-			console.log(store);
+var ImageList = Backbone.Collection.extend({
+	model: Image,
+	fetch: function(params) {
+		this.trigger('fetchstart');
+		var self = this;
+		_500px.api('/photos', params, function(response) {
+			console.log(response.data);
+			if (params.append) {
+				_.each(response.data.photos, function(photo) {
+					self.add(photo);
+				});
+			} else {
+				self.reset(response.data.photos);
+			}
+			delete response.data.photos;
+			var template = params.nextURL ? params.nextURL : "#photos/<%= feature %>/<% print(current_page + 1) %>";
+			var nextURL = _.template(template, response.data);
+			self.trigger('fetchend', response.data, nextURL);
 		});
-	 }
-});
-
-App.IndexRoute = Ember.Route.extend({
-	photos: function() {
-		var store = this.get('store');
-		//store.findQuery('photo', {feature: 'user', username: 'JoshuaDavis1'});
-		//console.log(store);
-		return ['1', '2'];
 	}
 });
+
+var Images = new ImageList;
+
+var AppView = Backbone.View.extend({
+	el: $('#app'),
+
+	events: {
+	},
+
+	initialize: function() {
+		//Attached events that occur outside the framework
+		$(window).on('resize', $.proxy(this.normalizeView, this));
+		this.authUser();
+
+		this.on('authorization_obtained', this.authUser);
+		this.$photolist = $("#photolist");
+		this.$showmore = $("#show-more");
+		this.$lightbox = $("#lightbox");
+		this.listenTo(Images, 'fetchend', this.renderImageList);
+		this.listenTo(Images, 'fetchstart', this.clearImageList);
+		this.normalizeView();
+	},
+
+	clearImageList: function() {
+		this.$showmore.addClass('loading');
+	//	this.$photolist.html('');
+	},
+
+	renderImageList: function(data, nextURL) {
+		this.$photolist.html('');
+		this.$showmore.attr('href', nextURL).removeClass('loading');
+		Images.each(function(image) {
+			var view = new ImageView({model: image});
+			this.$photolist.append(view.render().el);
+		}, this);
+	},
+
+	normalizeView: function() {
+		this.$photolist.width( this.fitContent(290) );
+	},
+
+	fitContent: function(width) {
+		return Math.floor( $(window).width() / width ) * width;
+	},
+
+	authUser: function() {
+
+	},
+
+	showPhoto: function(id) {
+		var view = new LightboxImageView({
+			model: Images.get(id)
+		});
+		//Arbitrary CSS hack to make popping the lightbox work on mobile safari... have no idea why this works
+		this.$lightbox.html(view.render().el).css('kerning', 0);
+	}
+});
+
+
+var Router = Backbone.Router.extend({
+	routes: {
+		"": "index",
+		"photos/user_favorites(/:user_id)(/:page)": "favorites",
+		"photos/:feature(/:user_id)": "photolist",
+		"photo/:id": "photo",
+	},
+
+	index: function() {
+		this.photolist('popular', 0);
+	},
+
+	photolist: function(feature, page) {
+		Images.fetch({
+			"feature": feature,
+			"image_size": "[3, 5]",
+			"page" : page ? page : 0,
+			"append": !!page,
+		})
+	},
+
+	photo: function(id) {
+		App.showPhoto(id);
+	},
+
+	favorites: function(userId, page) {
+		if(!userId || userId == 0) {
+			userId = App.me.id;
+		}
+
+		Images.fetch({
+			"feature": "user_favorites",
+			"sort": "created_at",
+			"image_size": "[3, 5]",
+			"user_id": userId,
+			"page" : page ? page : 0,
+			"append": page == 0 ? false : true,
+			"nextURL": "#photos/<%= feature %>/<%= filters.user_id %>/<% print(current_page + 1) %>"
+		})
+	}
+});
+
+var route = new Router();
+
+var App = new AppView();
+Backbone.history.start();
